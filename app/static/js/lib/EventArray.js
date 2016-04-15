a346781334b62213592606a3cafa9d3dfb7d3a64
radbitt <leo@radbitt.com>
2016-04-15 14:19:07 -0700

    Extends!
    Made Timeline and Events object extend EventArray
    
    Just like 111C taught us to do.

---
+++ app/static/js/lib/EventArray.js
@@ -0,0 +1,53 @@
+function EventArray() {
+
+	this.eventsArray = [];
+
+	this.firstEventInt = 0; 
+
+	this.currentEventInt = this.firstEventInt;
+
+	this.getName = function() {
+		return this.name; 
+	}
+
+	this.numOfEvents = function() {
+		return this.eventsArray.length; 
+	}
+
+	this.addEvent = function(date, category, string) {
+		var newEvent = new Event(date, category, string);
+		this.eventsArray.push(newEvent);
+		return newEvent;  
+	}
+
+	this.firstEvent = function() {
+		this.currentEventInt = this.firstEventInt;
+		return this.eventsArray[this.currentEventInt]; 
+	}
+
+	this.lastEvent = function() {
+        this.currentEventInt = this.eventsArray.length-1;
+        return this.eventsArray[this.currentEventInt]; 
+    }
+
+	this.currentEvent = function() {
+		return this.eventsArray[this.currentEventInt]; 
+	}
+
+	this.nextEvent = function() {
+		this.currentEventInt++;
+		if (this.currentEventInt >= this.getNumOfEvents())
+			this.currentEventInt = this.firstEventInt; 
+		return this.eventsArray[this.currentEventInt]; 
+	}
+
+	this.prevEvent = function() {
+		this.currentEventInt--;
+		return this.eventsArray[this.currentEventInt]; 
+	}	
+
+	this.copyOfArray = function() {
+		// return this.eventsArray;
+	}
+	
+}
--- app/static/js/lib/Timeline.js
+++ app/static/js/lib/Timeline.js
@@ -5,52 +5,9 @@ function Timeline() {
     // Array of different arrays of events
     this.eventsGroupArray = [];
 
-    // Array of exploded 
-    this.timelineArray = [];
-
-    this.firstEventInt = 0; 
-
-    this.currentEventInt = this.firstEventInt;
-
     this.addEventsGroup = function(EventsObject) {
         this.eventsGroupArray.push(EventsObject);
         return EventsObject; 
-    }
-
-    this.numOfEvents = function() {
-        return this.timelineArray.length; 
-    }
-
-    this.firstEvent = function() {
-        this.currentEventInt = this.firstEventInt;
-        return this.timelineArray[this.currentEventInt]; 
-    }
-
-    this.lastEvent = function() {
-        this.currentEventInt = this.timelineArray.length-1;
-        return this.timelineArray[this.currentEventInt]; 
-    }
-
-    this.currentEvent = function() {
-        return this.timelineArray[this.currentEventInt]; 
-    }
-
-    this.nextEvent = function() {
-        this.currentEventInt++;
-        if (this.currentEventInt >= this.numOfEvents())
-            this.currentEventInt = this.firstEventInt; 
-        return this.timelineArray[this.currentEventInt]; 
-    }
-
-    this.prevEvent = function() {
-        this.currentEventInt--;
-        if (this.currentEventInt <= 0)
-            this.currentEventInt = this.firstEventInt; 
-        return this.timelineArray[this.currentEventInt]; 
-    }   
-
-    this.copyOfArray = function() {
-        // return this.eventsGroupArray;
     }
 
     this.sortIntoTimeline = function(EventsObject) {
@@ -58,3 +15,6 @@ function Timeline() {
     }
 
 }
+
+Timeline.prototype = new EventArray();
+Timeline.prototype.constructor = Timeline; 
