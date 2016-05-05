5a536ede9ef94497a6856395ad4579153f59c816
Lutalo <wongstevenj@gmail.com>
2016-05-05 15:52:24 -0700

    Added navbar BAR

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -14,24 +14,28 @@ function navHighlight()
 
     if (active === "")
     {
-        document.getElementById("home").setAttribute("class", "active");
+            // document.getElementById("home").setAttribute("class", "active");
         //  console.log("hit home case");
+        document.getElementById("home").style.borderTop = "2px solid #4B90E0";
     }
 
     else if (active == "support" || active == "testing")
     {
         //  console.log("before services");
-        document.getElementById("services").setAttribute("class", "active");
+        // document.getElementById("services").setAttribute("class", "active");
+        document.getElementById("services").style.borderTop = "2px solid #4B90E0";
     }
 
     else if (active == "aboutHIV" || active == "prevention" || active == "videos")
     {
-        document.getElementById("information").setAttribute("class", "active");
+        document.getElementById("information").style.borderTop = "2px solid #4B90E0";
+        // document.getElementById("information").setAttribute("class", "active");
         //  console.log("hit info");
     }
     else
     {
-        document.getElementById(active).setAttribute("class", "active");
+        document.getElementById(active).style.borderTop = "2px solid #4B90E0";
+        // document.getElementById(active).setAttribute("class", "active");
         //  console.log("hit else statement");
     }
 }
