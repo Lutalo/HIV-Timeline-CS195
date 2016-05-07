2a4883d2651f3001a1261e1d483d3f4f1a86e5dd
Lutalo <wongstevenj@gmail.com>
2016-05-07 13:18:34 -0700

    fix #4

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -16,25 +16,26 @@ function navHighlight()
     {
             // document.getElementById("home").setAttribute("class", "active");
         //  console.log("hit home case");
-        document.getElementById("home").style.borderTop = "2px solid #4B90E0";
+        document.getElementById("home").style.borderTopColor = "#4B90E0";
     }
 
     else if (active == "support" || active == "testing")
     {
         //  console.log("before services");
         // document.getElementById("services").setAttribute("class", "active");
-        document.getElementById("services").style.borderTop = "2px solid #4B90E0";
+        document.getElementById("services").style.borderTopColor = "#4B90E0";
     }
 
     else if (active == "aboutHIV" || active == "prevention" || active == "videos")
     {
-        document.getElementById("information").style.borderTop = "2px solid #4B90E0";
+        document.getElementById("information").style.borderTopColor = "#4B90E0";
         // document.getElementById("information").setAttribute("class", "active");
         //  console.log("hit info");
     }
     else
     {
-        document.getElementById(active).style.borderTop = "2px solid #4B90E0";
+        document.getElementById(active).style.borderTopColor = "#4B90E0";
+        // document.getElementById(active).style.borderTop = "2px solid #4B90E0";
         // document.getElementById(active).setAttribute("class", "active");
         //  console.log("hit else statement");
     }
